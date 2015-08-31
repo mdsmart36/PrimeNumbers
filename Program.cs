4bbad1a04caf00b157878c33db0ccef4bcf1259f
Matt Smart <mdsmart36@gmail.com>
2015-08-31 07:01:59 -0500

    function to find prime numbers

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter limit: ");
+            Console.WriteLine("number of primes is {0}", getPrimes(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadLine();
+        }
+
+        static int getPrimes(int n)
+        {
+            int numberOfPrimes = 0;
+            for (int i = 2; i < n; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j < i-1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                    } 
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine("found a prime {0}", i);
+                    numberOfPrimes++;
+                   
+                }
+                Console.WriteLine(i);
+
+            }
+
+            return numberOfPrimes;
+        }
+    }
+}
